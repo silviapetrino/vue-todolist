c48d620ee54692566d231d2346fc82406bc94f62
silviapetrino <petrino.silvia@gmail.com>
2023-09-29 14:23:07 +0200

    [add]: list

--- js/script.js
+++ js/script.js
@@ -1,8 +1,41 @@
+
+// Istruzioni:
+
+// 1. Creare un markup statico in html/css;
+
+// 2. Inizializzare Vue;
+
+// 3. creare un array di oggetti che rappresentano le tasks;
+
+
+
 const { createApp } = Vue;
 
 createApp ({
 
+ data(){
+  return{
+    tasks: [
+      {
+        text: "Esercizi Boolean 0",
+        done: false
+      },
+      {
+        text: "Esercizi Boolean 1",
+        done: false
+      },
+      {
+        text: "Esercizi Boolean 2",
+        done: false
+      },
+      {
+        text: "Esercizi Boolean 3",
+        done: false
+      }
 
+    ]
+  }
+ }
 
 
 }).mount("#app")
